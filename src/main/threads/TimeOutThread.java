d327265d5d13ec3adc491c44363430512e0f732a
Yunju07 <juile0827@naver.com>
2025-02-19 00:46:19 +0900

    feat(treads) 타이머 스레드 구현

---
+++ src/main/threads/TimeOutThread.java
@@ -0,0 +1,25 @@
+package threads;
+
+public class TimeOutThread extends Thread{
+    private int timeout;    // 초 단위
+    private Thread targetThread;
+
+    public TimeOutThread(int timeout, Thread targetThread) {
+        this.timeout = timeout;
+        this.targetThread = targetThread;
+    }
+
+    public void run() {
+        try {
+            Thread.sleep(timeout * 1000);
+            System.out.println("\n"+ timeout + "초 간 응답이 없어 프로그램이 종료됩니다.");
+            targetThread.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopTimer() {
+        this.interrupt();
+    }
+}
